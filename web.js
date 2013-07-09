5892d493dce53aabcbfb6e565a64924883de4f4d
TheZelja <zelja.siroki@gmail.com>
2013-07-09 07:05:05 +0000

    Edited web.js to reead from index.html (hope so)

--- web.js
+++ web.js
@@ -3,7 +3,26 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+
+
+// Read the file and print its contents.
+var fs = require('fs')
+  , filename = 'index.html';
+fs.readFile(filename, 'utf8', function(err, data) {
+  if (err) throw err;
+  console.log('Finished file: ' + filename);
+  console.log(data)
+});
+
+
+response.send (data);
+
+
+///////  response.send('Hello World 2!');
+
+
+
 });
 
 var port = process.env.PORT || 5000;
